cd852263a884c219e5daa97686baeee76544640b
Jonathan MacDuff <jonathan.macduff@outlook.com>
2024-05-12 00:51:32 -0700

    completed lab

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,34 +1,103 @@
 class Game:
+
+    all = []
+
     def __init__(self, title):
-        self.title = title
+        self._title = title
+        Game.all.append(self)
+
+    @property
+    def title(self):
+        return self._title
+    
+    @title.setter
+    def title(self, title):
+        if isinstance(title, str) and len(title) > 0 and not hasattr(self, "_title"):
+            self._title = title
 
     def results(self):
-        pass
+        return [result for result in Result.all if result.game == self]
 
     def players(self):
-        pass
+        players = []        
+        for result in Result.all:
+            if result.game == self:
+                if not players.__contains__(result.player):
+                    players.append(result.player)
+        return players
+
 
     def average_score(self, player):
-        pass
+        scores_total = 0
+        scores_count = 0
+        for result in Result.all:
+            if result.game == self and result.player == player:
+                scores_total += result.score
+                scores_count += 1
+        return scores_total/scores_count
 
 class Player:
+
+    all = []
+
     def __init__(self, username):
-        self.username = username
+        self._username = username
+        Player.all.append(self)
+
+    @property
+    def username(self):
+        return self._username
+    
+    @username.setter
+    def username(self, username):
+        if isinstance(username, str) and 2 <= len(username) <= 16:
+            self._username = username
 
     def results(self):
-        pass
+        return [result for result in Result.all if result.player == self]
 
     def games_played(self):
-        pass
+        games = []        
+        for result in Result.all:
+            if result.player == self:
+                if not games.__contains__(result.game):
+                    games.append(result.game)
+        return games
 
     def played_game(self, game):
-        pass
+        games = self.games_played()
+        return games.__contains__(game)
 
     def num_times_played(self, game):
-        pass
+        game_count = 0
+        for result in Result.all:
+            if result.player == self and result.game == game:
+                game_count += 1
+        return game_count
 
 class Result:
+
+    all = []
+
     def __init__(self, player, game, score):
-        self.player = player
-        self.game = game
-        self.score = score
+        self._player = player
+        self._game = game
+        self._score = score
+        Result.all.append(self)
+
+    @property
+    def score(self):
+        return self._score
+    
+    @score.setter
+    def score(self, score):
+        if isinstance(score, int) and 1 <= score <= 5000 and not hasattr(self, "_score"):
+            self._score = score
+
+    @property
+    def player(self):
+        return self._player
+    
+    @property
+    def game(self):
+        return self._game
